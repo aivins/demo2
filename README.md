0d80df4863347bc2610ad821112dfe7e84fa44e0
Andrew Ivins <andrew@ivins.id.au>
2022-06-02 14:11:40 +0800

    Made more betterer

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # This is the git demo
 
 Hello
+
+# This is the new branch
+It is!
--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3
 
-print("Hello, world!")
-print("This is a new version!")
-print("This is another output line")
+print("HELLO WORLD!")
